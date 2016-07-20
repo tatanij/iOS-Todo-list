e3e548c5d16b00983d63151ec09c7a2bb1960939
Jasmeen Tatani <tatani.j@jasmeens-mac.local>
2016-07-20 06:23:15 +0100

    implemented addition of new value function

--- New TD List/ViewController.swift
+++ New TD List/ViewController.swift
@@ -8,11 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    var items: [String] = [] //Initialised with an empty array.
 
+    @IBAction func addItem(sender: AnyObject) {
+    //Used to add a new item to the todo list
+        alert()
+        
+    }
+    
+    @IBOutlet var listTblView: UITableView! //The list of added items
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        listTblView.dataSource = self //Merges view with table view
+    
+    }
+    
+    //Following two functions required for DataSourse
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("listItem") as! ItemTableViewCell
+        cell.itemLabel.text = items[indexPath.row]//this will change the text to the current item in the table view in the array
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count //counts the number of items there are in the list
+        
+    }
+    
+    func alert(){
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .Alert)
+        
+        alert.addTextFieldWithConfigurationHandler{
+            //This will allow for a text field to be present which you can use to add values in to the array and thus the todo list
+            (textfield) in
+            textfield.placeholder = "Task Description"
+        }
+        
+        let add = UIAlertAction(title: "Add", style: .Default){
+            (action) in
+            let textfield = alert.textFields![0] as! UITextField
+            self.items.append(textfield.text!) //adds value from the text field into the array "!" is there to unwrap the text
+            self.listTblView.reloadData() //Should reload the table view once you have added a new item
+           // print("hello") //testing for add button
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel){
+            (alert) in
+            
+           // print ("hi")//testing to see if cancel button works
+        }
+        alert.addAction(add)
+        alert.addAction(cancel)
+        
+        presentViewController(alert, animated: true, completion: nil) // allows for annimation of add and cancel button
     }
 
     override func didReceiveMemoryWarning() {
